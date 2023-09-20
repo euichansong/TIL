a3355177bee55d2b72d775fc6417e112ff5560e9
euichansong <139303904+euichansong@users.noreply.github.com>
2023-09-20 20:10:52 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -2,13 +2,10 @@ t = int(input())
 for tc in range(t):
     n = int(input())
     clothdict = {}
-    cnt = 1
     for i in range(n):
         cloth, where = input().rstrip().split()
-        # clothdict[where] = cnt
-        # 옷의 종류 갯수
+        # 옷의 종류 갯수세기 없으면 1로 시작
         if where in clothdict.keys():
-
             clothdict[where] += 1
         else:
             clothdict[where] = 1
@@ -17,6 +14,7 @@ for tc in range(t):
     combi = 1
     #
     for v in clothdict.values():
+        # 모든 종류의 의류 입지 않는 경우 포함
         combi *= (v+1)
     # 아무것도 안입는 경우 빼기
     print(combi-1)
