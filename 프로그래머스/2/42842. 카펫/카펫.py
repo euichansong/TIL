c30ee6c37fbe8f1f08c409ccf877a2d4e9d845c6
euichansong <139303904+euichansong@users.noreply.github.com>
2024-01-21 20:26:52 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -7,8 +7,8 @@ def solution(brown, yellow):
         if total % se == 0:
             ga = total // se
             print(ga,se)
-            for i in range(1,ga-1):
-                for j in range(i,se-1):
+            for i in range(1,ga-1): # 최소 노랑 가로 길이 = 전체 가로 -2
+                for j in range(i,se-1): # 최소 노랑 세로 길이 = 전체 세로 -2
                     if i * j == yellow:
                         answer = [se,ga]
     
