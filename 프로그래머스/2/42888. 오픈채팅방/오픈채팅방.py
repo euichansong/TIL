d7db68cac0a8e7f97b95b9a90b32d9b91ee221de
euichansong <139303904+euichansong@users.noreply.github.com>
2024-01-28 21:48:34 +0900

    [level 2] Title: 오픈채팅방, Time: 151.58 ms, Memory: 46.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,36 @@
+def solution(record):
+    
+    namedict = {}
+    recordlist = []
+    for i in record:
+        space = list(map(str, i.split()))
+        recordlist.append((space[0],space[1]))
+        for j in range(len(space)):
+            
+            if space[0] == 'Enter':
+                namedict[space[1]] = space[2]
+            elif space[0] == 'Change':
+                namedict[space[1]] = space[2]
+        
+    answer = []
+    # for order, id in recordlist:
+    #     if order == 'Enter':
+    #         for k, v in namedict.items():
+    #             if k == id:
+    #                 word = v + '님이 들어왔습니다.'
+    #                 answer.append(word)
+    #     elif order == 'Leave':
+    #         for k, v in namedict.items():
+    #             if k == id:
+    #                 word = v + '님이 나갔습니다.'
+    #                 answer.append(word)
+                    
+    for order, id in recordlist:
+        if order == 'Enter':
+            word = namedict[id] + '님이 들어왔습니다.'
+            answer.append(word)
+        elif order == 'Leave':
+            word = namedict[id] + '님이 나갔습니다.'
+            answer.append(word)
+    # print(answer)
+    return answer
