8b5b80c5dd1c030b3ea588447a492b55cafccb04
euichansong <139303904+euichansong@users.noreply.github.com>
2024-03-20 21:27:40 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -6,24 +6,29 @@
 from itertools import combinations
 def solution(orders, course):
     dict = {}
+    # 단어 자르기
     for i in orders:
         slice = []
         for j in range(len(i)):
             slice.append(i[j])
         slice.sort()
+        # 조합 만들기
         for c in course:
             combi = list(combinations(slice,c))
+            # 딕셔너리 저장
             for com in combi:
                 if com in dict:
                     dict[com] += 1
                 dict.setdefault(com,1)
     answer = []
     bb = max(course)
+    # 정답 배열
     answerlist = [[] for _ in range(bb+1)]
-    
+    # 코스별 길이 
     for c in course:
         maxcount = 0
         for key, value in dict.items():
+		        # 길이가 코스와 같고, 2개 이상
             if len(key) == c and value >= 2:                       
                 if value > maxcount:
                     answerlist[c] = []
@@ -32,19 +37,12 @@ def solution(orders, course):
                 elif value == maxcount:
                     answerlist[c].append(key)
                     maxcount = value
-    print(answerlist)
-    
+    # 다시 합치기 (튜플상태 이기 때문)
     for c in course:
-        
-        print(answerlist[c])
         for w in answerlist[c]:
             word = ''
             for www in w:
                 word += www
-            print(word)
-
             answer.append(word)
     answer.sort()
-    print(answer)
-    
     return answer
