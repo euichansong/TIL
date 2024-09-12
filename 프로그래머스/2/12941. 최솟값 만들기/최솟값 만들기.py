4d636abdc7e8d90712f1a3a569fe1cd822722b84
euichansong <139303904+euichansong@users.noreply.github.com>
2024-09-12 22:41:47 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.44 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -0,0 +1,14 @@
+"""
+가장 큰수 * 가장 작은수?
+1000*1000이니까 백트레킹, 재귀 x
+
+"""
+def solution(A,B):
+    answer = 0
+    a = sorted(A)
+    b = sorted(B,reverse=True)
+    # print(a,b)
+    for i in range(len(a)):
+        answer += a[i]*b[i]
+
+    return answer
