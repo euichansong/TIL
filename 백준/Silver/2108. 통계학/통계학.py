9a9ec0ff4a6c08279c8df1eff561fb8c78d58242
euichansong <139303904+euichansong@users.noreply.github.com>
2024-11-06 22:16:00 +0900

    [Silver III] Title: 통계학, Time: 272 ms, Memory: 122732 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,33 @@
+import copy,sys
+input = sys.stdin.readline
+n = int(input())
+nl = [int(input()) for _ in range(n)]
+a = int(round(sum(nl) / n,0))
+b = nl.copy()
+b.sort()
+bb = b[n//2]
+
+dict = {}
+for i in range(n):
+    if b[i] not in dict:
+        dict[b[i]] = 1
+    else:
+        dict[b[i]] += 1
+
+mx = max(dict.values())
+mxl = []
+for i in dict:
+    if mx == dict[i]:
+        mxl.append(i)
+d = max(nl) - min(nl)
+
+print(a)
+print(bb)
+
+if len(mxl) > 1:
+    mxl.sort()
+    print(mxl[1])
+else:
+    print(mxl[0])
+
+print(d)
