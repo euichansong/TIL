e4312d1d902fbb8dfb27ab124aaeee3dd49b8185
euichansong <139303904+euichansong@users.noreply.github.com>
2025-01-12 13:58:53 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -1,60 +1,23 @@
 import sys
-from collections import deque
-input = sys.stdin.readline
-"""
-큐랑 비교, 리스트랑 비교
+input =sys.stdin.readline
+N = int(input())
+lst = list(map(int,input().split()))
+stack = []
+start = 1
 
-5
-5 2 4 1 3
-"""
-n = int(input())
-stu = list(map(int,input().split()))
+while lst or stack:
+    if stack and stack[-1] == start:
+        start+=1
+        stack.pop()
+        continue
+    elif lst:
+        now = lst.pop(0)
+        stack.append(now)
+    else:
+        break
 
-def pro(stu):
-    stu = deque(stu)
-    w = deque()
-    now = 0
-    while stu or w:
-        # print(w,now)
-        
-        if w and w[-1] == now + 1:
-            w.pop()
-            now += 1
-        elif stu:
-            p = stu.popleft()
-            w.append(p)
-        else:
-            return now
-        
-    return now
-
-ans = pro(stu)
-if ans == n:
+if start == N+1:
     print("Nice")
+
 else:
     print("Sad")
-
-# def pro(stu):
-#     stu = deque(stu)
-#     w = deque()
-#     now = 0
-#     p = stu.popleft()
-#     w.append(p)
-#     while w:
-#         if now+1 == w[-1]:
-#             w.pop()
-#             now += 1
-#         if stu:
-#             p = stu.popleft()
-#             w.append(p)
-#             if now+1 == w[-1]:
-#                 w.pop()
-#                 now += 1
-#         # 줄 서있는거 없고 스택 마지막이 다르면
-#         if not stu:
-#             if w and w[-1] != now+1:
-#                 return now
-            
-#     return now
-
-
